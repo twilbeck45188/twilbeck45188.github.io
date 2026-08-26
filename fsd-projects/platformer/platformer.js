ef6f0339697f10c4c63ac642225ab22b670dc0a9
twilbeck45188 <twilbeck45188@students.apsb.org>
2026-08-26 19:12:53 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,28 +27,47 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
-
+    toggleGrid();
 
     // TODO 2 - Create Platforms
+    createPlatform(0,700,100,8,"hotpink");
 
+    createPlatform(100,600,200,8,"hotpink");
 
+    createPlatform(200,500,300,8,"hotpink");
+    createPlatform(500,500,8,200,"hotpink");
+    createPlatform(600,700,200,8,"hotpink");
+    createPlatform(700,600,100,8,"hotpink");
+    createPlatform(900,500,100,8,"hotpink");
+    createPlatform(1100,400,100,8,"hotpink");
+    createPlatform(700,300,300,8,"hotpink");
+    createBadPlatform(500,700,100,8,"red");
+    createPlatform(300, 150, 200, 20, "hotpink", 0, 0, 0, 210, 500, 1);
+    createPlatform(0,100,75,8,"hotpink");
+    createPlatform(200,150,100,8, "hotpink");
+    createPlatform(1000,600,200,8,"hotpink");
 
 
     // TODO 3 - Create Collectables
-
-
-
+    createCollectable("database",600,650,0.3);
+    createCollectable("database",0,50);
+    createCollectable("database",950,250);
+  
     
     // TODO 4 - Create Cannons
+    createCannon("top", 200, 2500, 20, 10, 100, 600, 2);
+
+    createCannon("top", 600,2000,10,20,600,800);
+    createCannon("top",1300,3000);
 
 
-    
-    
+  
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
   }
 
+
   registerSetup(setup);
 });
+
